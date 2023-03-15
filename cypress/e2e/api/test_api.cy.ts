5bccfb1b7ba5f818a96184231930d7ecf3992d10
xandervisma <xander.degroot@visma.com>
2023-03-15 13:36:24 +0100

    Update test_api.cy.ts

--- cypress/e2e/api/test_api.cy.ts
+++ cypress/e2e/api/test_api.cy.ts
@@ -51,13 +51,13 @@ describe('brands api tests', () => {
     });
 
     it('add products', () => {
-        const productDetail = { 
+        const productDetail = {
             "name": "Andúril",
             "description": "The flame of the west",
-            "price": 9000.00,
+            "price": 90.00,
             "category_id": 1,
             "brand_id": 1,
-            "product_image_id": 1,
+            "product_image_id": 10,
             "is_location_offer": 1,
             "is_rental": 0
         };
