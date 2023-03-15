9c76596891786f6944672d70ddd50dccbd6bd9d7
xandervisma <xander.degroot@visma.com>
2023-03-15 11:51:54 +0100

    Update test_api.cy.ts

--- cypress/e2e/api/test_api.cy.ts
+++ cypress/e2e/api/test_api.cy.ts
@@ -41,4 +41,27 @@ describe('brands api tests', () => {
             }
         );
     });
+
+    it('GET image', () => {
+        const imageDetail = { 
+            "id": "0",
+        };
+
+        cy.request('GET', 'https://api.practicesoftwaretesting.com/images', imageDetail);
+    });
+
+    it('add products', () => {
+        const productDetail = { 
+            "name": "Andúril",
+            "description": "The flame of the west",
+            "price": 9000.00,
+            "category_id": 1,
+            "brand_id": 1,
+            "product_image_id": 1,
+            "is_location_offer": 1,
+            "is_rental": 0
+        };
+
+        cy.request('POST', 'https://api.practicesoftwaretesting.com/products', productDetail);
+    });
 });
