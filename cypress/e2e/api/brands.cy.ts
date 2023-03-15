bb6b20a1c1b746525df708996eeaf4faaede20c6
xandervisma <xander.degroot@visma.com>
2023-03-15 09:53:31 +0100

    Update brands.cy.ts

--- cypress/e2e/api/brands.cy.ts
+++ cypress/e2e/api/brands.cy.ts
@@ -1,7 +1,7 @@
 describe('brands api tests', () => {
     it('retreive brands', () => {
         cy.request('GET', 'https://api.practicesoftwaretesting.com/brands/1').then((response) => {
-            
+            expect(response.body.name).to.be('Brand name 1');
         });
     })
 });
